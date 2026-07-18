e2f251b7b098e3a24c6d5fa2ffb3309446efb4bb
Gabriel Braga <bielbraga37@outlook.com>
2026-07-18 20:53:39 -0300

    Criando o primeiro model, makemigrations e migrate

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from contact.models import Contact
 # Register your models here.
+
+@admin.register(Contact) #adiciona o model no admin do django
+class ContactAdmin(admin.ModelAdmin):
+    ...

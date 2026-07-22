55e7725a2a1861ccba86caa0777ee9f6b07e079c
Gabriel Braga <bielbraga37@outlook.com>
2026-07-21 23:40:12 -0300

    Criei o campo owner usando o model User

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from contact.models import Contact
+from contact.models import Category
 # Register your models here.
 
 @admin.register(Contact) #adiciona o model no admin do django
@@ -12,3 +13,10 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+@admin.register(Category) #adiciona o model no admin do django
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
+    
+   
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
@@ -9,6 +10,16 @@ from django.utils import timezone
 # picture (imagem)
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -19,7 +30,17 @@ class Contact(models.Model):
     descripton = models.TextField(blank=True)
     show = models.BooleanField(default=True) #se eu quero ou não exibir um contato
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
-def __str__(self) -> str:
-    return f'{self.first_name} {self.last_name}'
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
 
